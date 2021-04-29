c5f60926df44a0b35fd37d178dbde526ccc07e8c
Prabhakar Rai <student343.bhu@gmail.com>
2021-04-29 18:34:11 +0530

    7.14 blogs by an user

--- bloglist/src/components/Users.js
+++ bloglist/src/components/Users.js
@@ -1,17 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import {
+  Switch,
+  Link,
+  Route,
+  useRouteMatch
+} from 'react-router-dom';
 import userServices from '../services/users';
 
 const User = ({ userdata }) => {
   return (
     <tr>
-      <td>{userdata.name}</td>
+      <td>
+        <Link to={`/users/${userdata.id}`}>{userdata.name}</Link>
+      </td>
       <td>{userdata.blogs.length}</td>
     </tr>
   );
 };
 
+const UserBlogs = ({ userdata }) => {
+  if (userdata === null || userdata === undefined) {
+    return null;
+  }
+  return (
+    <div>
+      <h2>{userdata.name}</h2>
+      <p>Added Blogs</p>
+      <ul>
+        {userdata.blogs.map((b) => <li key={b.id}>{b.title}</li>)}
+      </ul>
+    </div>
+  );
+};
+
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const match = useRouteMatch('/users/:id');
+  const user = match
+    ? users.find((u) => u.id === match.params.id)
+    : null;
 
   useEffect(() => {
     const fetch = async () => {
@@ -22,20 +49,27 @@ const Users = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Users</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name of the User</th>
-            <th>Blogs Created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((u) => <User key={u.id} userdata={u} />)}
-        </tbody>
-      </table>
-    </div>
+    <Switch>
+      <Route path='/users/:id'>
+        <UserBlogs userdata={user} />
+      </Route>
+      <Route path='/users'>
+        <div>
+          <h1>Users</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Name of the User</th>
+                <th>Blogs Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((u) => <User key={u.id} userdata={u} />)}
+            </tbody>
+          </table>
+        </div>
+      </Route>
+    </Switch>
   );
 };
 
